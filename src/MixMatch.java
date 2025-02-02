7b9e4b85b3a51aa67dc6a2b3c44f7178f31c0666
camilarrez <161494674+camilarrez@users.noreply.github.com>
2025-02-02 01:25:43 +0000

    Second commit

--- src/MixMatch.java
+++ src/MixMatch.java
@@ -9,5 +9,16 @@ public class MixMatch{
         double z = 4.99;
         boolean flag = false;
 
-        System.out.println(greeting "a string of text such as 'good morning'"+ letter "a single character letter"+ short_num "small numbers ranging from -32,768 to -32,768"+ x ""+ long_num "stores larger numbers up to the quintillion(9,223,372,036,854,775,807)(requires l)"+ y "a number with a decimal precision of about 7(requires f)" + z "number with a decimal precision of about 15"+ flag "stores true or false");
+        System.out.println(greeting + " <- String, a string of text such as 'good morning'");
+        System.out.println(letter + " <- char, a single character letter");
+        System.out.println(short_num + " <- short, small numbers ranging from -32,768 to 32,768");
+        System.out.println(x + " <- integer, an integer number such as -3, 56, 190000");
+        System.out.println(long_num + " <- long, stores larger numbers up to the quintillion(9,223,372,036,854,775,807)(requires l)");
+        System.out.println(y + " <- float, a number with a decimal precision of about 7(requires f)");
+        System.out.println(z + " <- double, number with a decimal precision of about 15");
+        System.out.println(flag + " <- boolean, stores true or false");
+
+        System.out.println(greeting + " " + letter + " " + short_num + " " + x + " " + long_num + " " + y + " " + z + " " + flag);
+
+    }
 }
