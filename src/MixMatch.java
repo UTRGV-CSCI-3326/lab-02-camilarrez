ba8f38c16690837f75e2befc1eb41c800c269c5e
camilarrez <161494674+camilarrez@users.noreply.github.com>
2025-02-02 00:48:13 +0000

    first commit

---
+++ src/MixMatch.java
@@ -0,0 +1,13 @@
+public class MixMatch{
+    public static void main(String[] args){
+        String greeting = "Hello";
+        char letter = 'a';
+        short short_num = 50;
+        int x = 3;
+        long long_num = 2000000L;
+        float y = 1.4f;
+        double z = 4.99;
+        boolean flag = false;
+
+        System.out.println(greeting "a string of text such as 'good morning'"+ letter "a single character letter"+ short_num "small numbers ranging from -32,768 to -32,768"+ x ""+ long_num "stores larger numbers up to the quintillion(9,223,372,036,854,775,807)(requires l)"+ y "a number with a decimal precision of about 7(requires f)" + z "number with a decimal precision of about 15"+ flag "stores true or false");
+}
